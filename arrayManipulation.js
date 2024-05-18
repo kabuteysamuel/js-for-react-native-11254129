a2e3f4bd009a54bf6705848c1af46d99e231ee24
Kest08 <kabsnani@gmail.com>
2024-05-18 21:18:16 +0000

    Task2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,3 +9,24 @@ function processArray(arr){
     }
     return newArray;
 }
+function formatArrayStrings(array) {
+    if (!Array.isArray(array)) {
+        throw new TypeError("Input should be an array of strings");
+    }
+}
+function formatArrayStrings(strArr, numArr)  {
+    if(Array.isArray(strArr) ==!Array.isArray(numArr) ==!strArr.length !== numArr.length) {
+        throw new Error('Arrays should be of the same length');
+    } 
+    const formattedArr = [];
+for (let i=0; i < strArr.length; i++) {
+    const str=strArr[i];
+    const num=numArr[i];
+    if(num % 1/2 ===0) {
+        formattedArr.push(str.toUpperCase());
+    } else{
+        formattedArr.push(str.toLowerCase());
+    }
+ }
+return formattedArr;
+}
